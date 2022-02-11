ef1d05ddf0e2ad38f1add3bacca0cfa64ae9aac5
MihirKamble1 <98868101+MihirKamble1@users.noreply.github.com>
2022-02-11 10:54:58 +0530

    updated

--- 3_Implementation/FunGame.c
+++ 3_Implementation/FunGame.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-  struct play {  
+  struct Quiz {  
             char name[20];  
             int score;  
             int stars;       //star ratings  
-}p;  
+}q;  
   
 void high_score();  
 void start();  
@@ -42,15 +42,14 @@ void help()
 {  
      int ch;  
   
-     printf("\n\n ************************* GAME HELP *************************");  
-     printf("\n -------------------------------------------------------------------------");  
+      
      printf("\n ....................FunGame...........\n");  
      printf("\n >> There will be a total of 20 questions");  
      printf("\n >> You will be given 4 options and you have to press 1, 2 ,3 or 4 for the");  
      printf("\n    right option");  
      printf("\n >> Each question will carry 10 points");  
      printf("\n >> You will be asked questions continuously.");  
-     printf("\n >> There is no negative marking for wrong answer");  
+     printf("\n >> There will be negative marking for wrong answer 10 points will be deducted");  
   
     printf("\n\n ************************* BEST OF LUCK *************************\n\n");  
   
@@ -84,12 +83,12 @@ void start()
   
   
         printf("\nPlease enter your name: ");  
-        scanf("%s",p.name);  
+        scanf("%s",q.name);  
   
   
   
   
-        printf("******************* Welcome \" %s \"  to C Quiz Game *************************** \n\n", p.name);  
+        printf("******************* Welcome \" %s \" *************************** \n\n", q.name);  
   
   
         printf("Q(1).What is git \n 1.Open Source Project \t\t2.Distributed Version Control System \n 3.Secured By SHA1\t\t 4.All of the above\n\n");  
@@ -105,7 +104,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 4.All of the above\n\n");  
+            printf("Wrong answer! Correct answer is 4.All of the above\n\n");
+          --count;
         }  
   
         printf("Q(2) Which vendor acquired GitHub for $7.5 billion in June 2018? \n 1.Oracle\t\t 2.Microsoft \n3.Google\n4.IBM\n\n");  
@@ -122,6 +122,7 @@ void start()
         else  
         {  
             printf("Wrong answer! Correct answer is 2.Microsoft\n\n");
+          --count;
               
         }  
   
@@ -139,6 +140,7 @@ void start()
         else  
         {  
             printf("Wrong answer! Correct answer is 2.User\n\n");
+          --count;
               
         }  
   
@@ -155,6 +157,7 @@ void start()
         else  
         {  
             printf("Wrong answer! Correct answer is 2.Diphu, Assam\n\n");
+          --count;
               
         }  
   
@@ -169,7 +172,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 4.All of the above\n\n");  
+            printf("Wrong answer! Correct answer is 4.All of the above\n\n");
+          --count;
         }  
   
         printf("Q(6).Hitler party which came into power in 1933 is known as \n1.Labour Party\t2.Nazi Party\n3.Ku-Klux-Klan\t4.Democratic Party\n\n");  
@@ -183,7 +187,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 2.Nazi Party\n\n");  
+            printf("Wrong answer! Correct answer is 2.Nazi Party\n\n");
+          --count;
         }  
   
         printf("Q(7). FFC stands for \n1.Foreign Finance Corporation\t2.Film Finance Corporation\n3.Federation of Football Council\t4.None of the above\n\n");  
@@ -197,7 +202,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 2.Film Finance Corporation\n\n");  
+            printf("Wrong answer! Correct answer is 2.Film Finance Corporation\n\n");
+          --count;
         }  
   
         printf("Q(8). Fastest shorthand writer was \n1.Dr. G. D. Bist\t2. J.R.D. Tata\n3. J.M. Tagore\t4. Khudada Khan\n\n");  
@@ -211,7 +217,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 1.Dr. G. D. Bist\n\n");  
+            printf("Wrong answer! Correct answer is 1.Dr. G. D. Bist\n\n");
+          --count;
         }  
   
         printf("Q(9). Epsom (England) is the place associated with \n1.Horse racing\t2.Polo\n3.Shooting\t4.Snooker\n\n");  
@@ -225,7 +232,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 1.Horse racing\n\n");  
+            printf("Wrong answer! Correct answer is 1.Horse racing\n\n");
+          --count;
         }  
   
         printf("Q(10). First human heart transplant operation conducted by Dr. Christian Bernard on Louis Washkansky, was conducted in \n1.1967\t2.1968\n3.1958\t4.1922\n\n");  
@@ -239,7 +247,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 1. 1967\n\n");  
+            printf("Wrong answer! Correct answer is 1. 1967\n\n");
+          --count;
         }  
   
         printf("Q(11). Frames from one LAN can be transmitted to another LAN via the device \n1. Router\t2. Bridge\n3. Repeater\t4. Modem\n\n");  
@@ -253,7 +262,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 2. Bridge\n\n");  
+            printf("Wrong answer! Correct answer is 2. Bridge\n\n");
+          --count;
         }  
   
         printf("Q(12). Which of the following condition is used to transmit two packets over a medium at the same time?\n1. Contention\t2.  Collision\n3.   Synchronous\tD. Asynchronous\n\n");  
@@ -267,7 +277,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 2. Collision\n\n");  
+            printf("Wrong answer! Correct answer is 2. Collision\n\n");
+          --count;
         }  
   
         printf("Q(13). What are the most commonly used transmission speeds in BPS used in data communication?\n1.   300\t2. 1200\n3.   2400\t4. 9600\n");  
@@ -281,7 +292,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 4. 9600\n\n");  
+            printf("Wrong answer! Correct answer is 4. 9600\n\n");
+          --count;
         }  
   
         printf("Q(14).  Which of the following is used for modulation and demodulation?\n1. modem\t2. protocols\n3. gateway\t4. multiplexer\n");  
@@ -295,7 +307,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is 1.modem\n\n");  
+            printf("Wrong answer! Correct answer is 1.modem\n\n");
+          --count;
         }  
   
         printf("Q(15). Contention is \n1. One or more conductors that serve as a common connection for a related group of devices\n2.   a continuous frequency capable of being modulated or impressed with a second signal\n3.  the condition when two or more stations attempt to use the same channel at the same time\n4.   a collection of interconnected functional units that provides a data communications service among stations attached to the network\n");  
@@ -309,7 +322,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  3. the condition when two or more stations attempt to use the same channel at the same time\n\n");  
+            printf("Wrong answer! Correct answer is  3. the condition when two or more stations attempt to use the same channel at the same time\n\n");
+          --count;
         }  
   
         printf("Q(16). When you ping the loopback address, a packet is sent where?\n1. On the network\n2. Down through the layers of the IP architecture and then up the layers again\n3. Across the wire\n4. through the loopback dongle\n");  
@@ -323,7 +337,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  2. Down through the layers of the IP architecture and then up the layers again\n\n");  
+            printf("Wrong answer! Correct answer is  2. Down through the layers of the IP architecture and then up the layers again\n\n");
+          --count;
         }  
   
         printf("Q(17). Which of the following TCP/IP protocol is used for transferring electronic mail messages from one machine to another?\n1. FTP\t2.    SNMP\n3. SMTP\t4. RPC\n");  
@@ -337,7 +352,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  3. SMTP\n\n");  
+            printf("Wrong answer! Correct answer is  3. SMTP\n\n");
+          --count;
         }  
   
         printf("Q(18). Which of the following device is used to connect two systems, especially if the systems use different protocols?\n1. hub\t2. bridge\n3. gateway\t4. repeater\n");  
@@ -351,7 +367,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  3. gateway\n\n");  
+            printf("Wrong answer! Correct answer is  3. gateway\n\n");
+          --count;
         }  
   
         printf("Q(19). The slowest transmission speeds are those of\n1. twisted-pair wire\t2. coaxial cable\n3. fiber-optic cable\t4.   microwaves\n");  
@@ -365,7 +382,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  1. twisted-pair wire\n\n");  
+            printf("Wrong answer! Correct answer is  1. twisted-pair wire\n\n");
+            --count;
         }  
   
         printf("Q(20). What is the port number for NNTP?\n1. 119\t2. 80\n3. 79\t4. 70\n");  
@@ -379,7 +397,8 @@ void start()
         }  
         else  
         {  
-            printf("Wrong answer! Correct answer is  1. 119 \n\n");  
+            printf("Wrong answer! Correct answer is  1. 119 \n\n");
+            --count;
         }  
   
   
@@ -388,37 +407,38 @@ void start()
         {  
                 printf("Thanks for playing, Your scored: %d points \t", count*10);  
   
-                p.score=count*10;  
+                q.score=count*10;
+                
   
-                if(p.score >= 80)  
+                if(q.score >= 80)  
                 {  
                     printf("Rating: * * * * *");  
-                    p.stars=5;  
+                    q.stars=5;  
                 }  
-                else if(p.score >= 60 && p.score < 80)  
+                else if(q.score >= 60 && q.score < 80)  
                 {  
                     printf("Rating: * * * *");  
-                    p.stars=4;  
+                    q.stars=4;  
                 }  
-                else if(p.score >= 40 && p.score < 60)  
+                else if(q.score >= 40 && q.score < 60)  
                 {  
                     printf("Rating: * * *");  
-                  p.stars=3;  
+                  q.stars=3;  
                 }  
-                else if(p.score >= 20 && p.score < 40)  
+                else if(q.score >= 20 && q.score < 40)  
                 {  
                     printf("Rating: * *");  
-                    p.stars=2;  
+                    q.stars=2;  
                 }  
-                else if(p.score < 20)  
+                else if(q.score < 20)  
                 {  
                     printf("Rating: *");  
-                     p.stars=1;  
+                     q.stars=1;  
                 }  
   
             //writing to file  
   
-                    fprintf(fp,"%s %d %d", p.name,p.score,p.stars);  
+                    fprintf(fp,"%s %d %d", q.name,q.score,q.stars);  
                     fclose(fp);  
         }  
         else  
@@ -443,10 +463,10 @@ void high_score()
   
     printf("\n******************************* HIGH SCORES *******************************\n\n");  
     printf("NAME     POINTS     RATING\n");  
-    while(fscanf(fp,"%s %d %d",p.name,&p.score, &p.stars) !=EOF)  
+    while(fscanf(fp,"%s %d %d",q.name,&q.score, &q.stars) !=EOF)  
     {  
         printf("____________________________\n");  
-        printf("%s     %d     %d star(s)\n\n",p.name,p.score,p.stars);  
+        printf("%s     %d     %d star(s)\n\n",q.name,q.score,q.stars);  
     }  
   
     fclose(fp);  
